87316d07f8fae52a15bc175224864854c0b329ba
AltynaiUrmatbekova <goldmoon1090@gmail.com>
2023-05-15 11:54:45 +0600

    Created AddProduct and AddToCart components

--- src/Drawer/Drawer.js
+++ src/Drawer/Drawer.js
@@ -1,19 +1,19 @@
-// import Logo from "../Logo/Logo";
-// import Nav from "../Nav/Nav";
-// import "./Drawer.css"
+import Logo from "../Logo/Logo";
+import Nav from "../Nav/Nav";
+import "./Drawer.css"
 
-// function Drawer({ open, toggle }) {
-//   const drawerClassNames = `Drawer ${open ? "open" : ""}`;
+function Drawer({ open, toggle }) {
+  const drawerClassNames = `Drawer ${open ? "open" : ""}`;
 
-//   return (
-//     <div className={drawerClassNames}>
-//       <div onClick={toggle} className="backdrop"></div>
-//       <div className="content">
-//         <Logo />
-//         <Nav />
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div className={drawerClassNames}>
+      <div onClick={toggle} className="backdrop"></div>
+      <div className="content">
+        <Logo />
+        <Nav />
+      </div>
+    </div>
+  );
+}
 
-// export default Drawer; 
+export default Drawer; 
